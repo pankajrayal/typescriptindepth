57b3b69f1b02120bef948fed96437755602a8b2b
Pankaj Rayal <pankaj_rayal@hotmail.com>
2017-03-09 16:25:23 -0500

    03092017v2

---
+++ typescriptindepth/03_TypeScriptBasic/HelloWorld.js
@@ -0,0 +1,12 @@
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Hello World!');
+        console.log('Hello World 2');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=HelloWorld.js.map
--- typescriptindepth/03_TypeScriptBasic/HelloWorld.ts
+++ typescriptindepth/03_TypeScriptBasic/HelloWorld.ts
@@ -1,6 +1,7 @@
 class Startup{
     public static main(): number{
         console.log('Hello World!');
+        console.log('Hello World 2');
         return 0;
     }
 }
